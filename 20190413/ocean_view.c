8f305176d4abfd47c2d4d5d0e266c6ad4af29536
stomita083 <s-tomita@unext.jp>
2019-04-16 13:56:46 +0900

    modify b

--- 20190413/ocean_view.c
+++ 20190413/ocean_view.c
@@ -6,23 +6,22 @@ int main()
     scanf("%d", &num);
     
     int heights[20];
-    int ans[20];
-    // 
+
+    // 入力値の読み込み
     for(int i = 0; i < num; i ++) {
         scanf("%d", &heights[i]);
     }
+
     // 判定
     int j = 0;
-    for(int i = 1; i < num; i ++) {
-        int t = heights[i];
-        if(t >= heights[0] && t >= heights[1]) {
-            if(t >= heights[i-1]) {
-                ans[j] = t;
-                j++;
-            }
+    int t = 0;
+    for(int i = 0; i < num; i ++) {
+        if(t <= heights[i]) {
+            t = heights[i];
+            j++;
         }
     }
     // 出力
-    printf("%d¥n", j+1);
+    printf("%d", j);
     return 0;
 }
